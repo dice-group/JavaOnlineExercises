3f0870759dbcbe14894dbd3404d43c94f100f4d6
Adrian Wilke <adibaba@users.noreply.github.com>
2018-08-24 14:09:26 +0200

    Minor upgrades to shell scripts

--- diceapp/scripts/build.sh
+++ diceapp/scripts/build.sh
@@ -1,3 +1,4 @@
-sudo docker build -t diceapp:latest . > build.log
+echo "Monitor the progress with: tail -f -n 20 build.log"
+docker build -t diceapp:latest . >> build.log
 echo "Please set the passwords for users nbgadmin and nbguser manually"
 echo "usermod --password \$(openssl passwd -1 <PSW>) <USR>"
--- diceapp/scripts/run.sh
+++ diceapp/scripts/run.sh
@@ -1,7 +1,14 @@
+# This file publishes the nbgrader container port 8000 to the host port 8000
+# It mounts the database directory for backups
+# It mounts the home directory for backups
 docker run \
 -t -d \
 -p 8000:8000 \
 -v /mnt/database:/srv/jupyterhub \
--v /mnt/adminhome/:/home \
+-v /mnt/home:/home \
 --name diceapp \
 diceapp
+
+# Version for HTTPS
+# This file publishes the nbgrader container port 8000 to the HTTPS host port 443
+#-p 443:8000 \
