6aa0d54a87df6b99f904c8aa1ca0644a171e6a3a
Adrian Wilke <adibaba@users.noreply.github.com>
2018-08-27 15:32:03 +0200

    Dockerfile: HTTPS added. Scripts: Added variables

--- diceapp/scripts/bash.sh
+++ diceapp/scripts/bash.sh
@@ -1,1 +1,7 @@
-docker exec --interactive --tty diceapp /bin/bash
+#!/bin/bash
+
+# This file executes a command to access a bash inside a docker container.
+
+CONTAINER_NAME="diceapp"
+
+docker exec --interactive --tty $CONTAINER_NAME /bin/bash
--- diceapp/scripts/run.sh
+++ diceapp/scripts/run.sh
@@ -1,14 +1,22 @@
-# This file publishes the nbgrader container port 8000 to the host port 8000
-# It mounts the database directory for backups
-# It mounts the home directory for backups
+#!/bin/bash
+
+# This file builds and runs a docker container.
+# It publishes a nbgrader container port to a host port.
+# It mounts the home directory for backups.
+# It mounts the database directory for backups.
+
+IMAGE_NAME="diceapp"
+TAG_NAME="latest"
+CONTAINER_NAME="diceapp"
+PORT_HOST="443"
+PORT_CONTAINER="8000"
+MOUNT_DIR_HOME="/tmp/mnt/database"
+MOUNT_DIR_DATABASE="/tmp/mnt/home"
+
 docker run \
 -t -d \
--p 8000:8000 \
--v /mnt/database:/srv/jupyterhub \
--v /mnt/home:/home \
---name diceapp \
-diceapp
-
-# Version for HTTPS
-# This file publishes the nbgrader container port 8000 to the HTTPS host port 443
-#-p 443:8000 \
+-p $PORT_HOST:$PORT_CONTAINER \
+-v $MOUNT_DIR_HOME:/srv/jupyterhub \
+-v $MOUNT_DIR_DATABASE:/home \
+--name $CONTAINER_NAME \
+$IMAGE_NAME:$TAG_NAME
